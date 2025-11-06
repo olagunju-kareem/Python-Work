727b6605869336007d9cd47fb22c43a8cafe135e
Kareem Olagunju <olagunjukareem111@gmail.com>
2025-11-06 17:15:17 +0100

    Update hangman.py
    Real hangman game

--- hangman.py
+++ hangman.py
@@ -1,125 +1,85 @@
 #! python3
-# ticTacToe.py - A program that models the classic Tic Tac Toe game.
- 
-"""INSTRUCTIONS:
-	The board format is as shown below:
-		1|2|3
-		-+-+-
-		4|5|6
-		-+-+-
-		7|8|9
-		Enter the number that's in the position you want to play in.
-		I hope you have fun playing."""
+# hangman.py - A program that models the classic hangman game. 
 
-BOARD = {
-	1: ' ', 
-	2: ' ', 
-	3: ' ',
-	4: ' ',
-	5: ' ',
-	6: ' ',
-	7: ' ',
-	8: ' ',
-	9: ' ',
-}
+import random, time, pyinputplus as pyip
+from string import ascii_letters
 
-def inputPos(prompt):
-	"""Ensures player inputs a valid position."""
+MAX_LIVES = 7
+WORDS = ['banana', 'uranium', 'upgrade', 'salmonella', 'caviar', 'beautiful', 'engineer', 'malevolent', 'kitchen', 'domestic', 'physics', 'spanish', 'intense', 'trouser', 'hangman', 'python', 'aeroplane', 'android', 'university', 'electricity', 'pillow', 'anaconda', 'jaguar']
+
+# Custom function to ensure one letter input:
+def oneLetter():
 	while True:
-		pos = input(prompt)
-		if pos.isdecimal():
-			x = int(pos)
-			if 1 <= x <= 9:
-				return x
-			else:
-				print('Please enter a number between 1 and 9.')
+		user_input = input('\nGuess a letter: ')
+		if len(user_input) == 1 and user_input in ascii_letters:
+			return user_input.lower()
 		else:
-			print('Invalid Input! Enter a number.')
-
-def winGame(board):
-	"""Helps to check for game win conditions."""
-	winPositions = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7),
-		            (2, 5, 8), (3, 6, 9), (3, 5, 7), (1, 5, 9)]
-	for positions in winPositions:
-		if board[positions[0]] == board[positions[1]] == board[positions[2]] != ' ':
-			return board[positions[0]]
-	return None
-		
-def printGame(board):
-	"""Prints game board."""
-	print(f'{board[1]}|{board[2]}|{board[3]}')
-	print('-+-+-')
-	print(f'{board[4]}|{board[5]}|{board[6]}')
-	print('-+-+-')
-	print(f'{board[7]}|{board[8]}|{board[9]}')
-
-def playAgain():
-	"""Asks if user wants to play again."""
-	while True:
-		again = input('Would you like to play again? (y/n) ')
-		if again in ['y', 'n']:
-			if again == 'y':
-				print('Welcome to another round of Tic Tac Toe.\n')
-				return True
-			else:
-				print('It was nice having you, byeeee.')
-				return False
-
-	
+			print('Invalid input! Please, enter a single letter.')
+			
+# Main game loop
 def main():
-	"""Main game."""
-	board = BOARD.copy()
-	turn = 'X'
-	x_score = 0
-	o_score = 0
-	# Display score board.
-	print(f'Player X: {x_score}     Player O: {o_score}')
-	# Display board for players:
-	printGame(board)
-	# Ensure game continues given that empty spaces remain.
-	while ' ' in board.values():
+	while True:
+		# Number of wrong guesses allowed till loss:
+		lives = MAX_LIVES
+	
+		print('Welcome to the classic Hangman game by Kareem 🎉')
+	
+		# Randomly select a  word from words list
+		game_word = random.choice(WORDS)
+	
+		# Print game representation of game wordp
+		print(f"Word to guess: {' '.join('_' * len(game_word))}")
+	
+		# Make a list to store correct guesses, and underscores to represent unguessed letters
+		c_guess = ['_' for i in range(len(game_word))]
+		# List to store all guesses:
+		guesses = []
+	
+		# Create loop to keep getting user input
 		while True:
-			if turn == 'X':
-				x_pos = inputPos('\nX turn: ')
-				if board[x_pos] == ' ':
-					board[x_pos] = 'X'
-					turn = 'O'
-					printGame(board)
-					break
-				else:
-					print('Position already occupied! Try another position.')
-			elif turn == 'O':
-				o_pos = inputPos('\n0 turn: ')
-				if board[o_pos] == ' ':
-					board[o_pos] = 'O'
-					turn = 'X'
-					printGame(board)
-					break
-				else:
-					print('Position already occupied! Try another position.')
+			# Get guess letter through the custom function and add to guesses list
+			guess_letter = oneLetter()
 		
-		# Check for win, and then prompt player for another round or not
-		win = winGame(board)
-		if win:
-			print(f'\nPlayer {win} wins.')
-			if win == 'X':
-				x_score += 1
+			# If guess is correct and has not been guessed before, add guesses word to correct guesses and print.
+			if (guess_letter in game_word) and (guess_letter not in c_guess):
+				print('Yay! You guessed right.')
+				for i, letter in enumerate(game_word):
+					if letter == guess_letter:
+						c_guess[i] = guess_letter
+				print(' '.join(c_guess))
+		
+			# If guess is correct but has already been guessed.
+			elif (guess_letter in guesses):
+				print('You have already guessed this letter. Please,try another letter.')
+		
+			# If guess is wrong, deduct lives.
 			else:
-				o_score += 1
-			print(f'Player X: {x_score}     Player O: {o_score}')
-			Again = playAgain()
-			if Again:
-				main()
-			else:
+				lives -= 1
+				if lives != 0:	
+					print('Incorrect guess, try again!')
+					print(f'You have {lives} lives left.')
+					print(' '.join(c_guess))
+				else:
+					print('Game Over, you have exhausted your lives.')
+					print(f'The correct word is "{game_word}".')
+					break
+		
+			# If all the '_' have been replaced i.e all letters correctly guessed, end game.
+			if '_' not in c_guess:
+				print('\nCongratulations, you finished the game!')
+				time.sleep(1)
+				print('Take this ₦10 make you use am hold body 😂.')
+				time.sleep(1)
+				print('₦10')
 				break
-
-	# If no more empty spaces and no win, then game is a tie.
-	else:
-		print('\n')
-		printGame(board)
-		print("It's a tie.")
-		Again = playAgain()
-		if Again:
-			main()
-
-main()
+			guesses.append(guess_letter)
+		
+		again = pyip.inputYesNo('Would you like to play again?\n')
+		if again == 'yes':
+			print('\nYAY!')
+			continue
+		else:
+			print('\nByeee,it seems you don\'t  want my ₦10 again 🌚.')
+			break
+			
+main()	
